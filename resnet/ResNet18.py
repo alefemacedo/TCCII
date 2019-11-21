1638b606695e0246138c6ba5bb42ee39080fd5dc
alefemacedo <almacedobr2@gmail.com>
2019-11-21 11:50:39 -0300

    Nova classe para modelo com arquitetura ResNet18

---
+++ resnet/ResNet18.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, Dropout
+from tensorflow.keras.models import Model
+from resnet.resnets_utils import *
+from tensorflow.keras.initializers import glorot_uniform
+"""matplotlib inline"""
+
+import tensorflow.keras.backend as K
+import tensorflow.keras as keras
+K.set_image_data_format('channels_last')
+K.set_learning_phase(1)
+
+def identity_block(X, filters, stage, block):
+    """
+    Implementation of the identity block
+    
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    
+    Returns:
+    X -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+    """
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2 = filters
+    
+    # Save the input value. You'll need this later to add back to the main path. 
+    X_shortcut = X
+    
+    # First component of main path (zero-padding)
+    X = Conv2D(filters = F1, kernel_size = (3, 3), strides = (1,1), padding = 'same', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = keras.regularizers.l2(0.001))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    # Second component of main path (≈2 lines)(zero-padding)
+    X = Conv2D(filters = F2, kernel_size = (3, 3), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = keras.regularizers.l2(0.001))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+    
+    
+    return X
+
+def convolutional_block(X, filters, stage, block, s = 2):
+    """
+    Implementation of the convolutional block as defined in Figure 4
+    
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    s -- Integer, specifying the stride to be used
+    
+    Returns:
+    X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
+    """
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2 = filters
+    
+    # Save the input value
+    X_shortcut = X
+
+
+    ##### MAIN PATH #####
+    # First component of main path (zero-padding)
+    X = Conv2D(F1, (3, 3), strides = (s,s), padding = 'same', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = keras.regularizers.l2(0.001))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    # Second component of main path (≈2 lines)(zero-padding)
+    X = Conv2D(filters = F2, kernel_size = (3, 3), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = keras.regularizers.l2(0.001))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+
+
+    ##### SHORTCUT PATH #### (≈2 lines)(zero-padding)
+    X_shortcut = Conv2D(filters = F2, kernel_size = (3, 3), strides = (s,s), padding = 'same', name = conv_name_base + '1',
+                        kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = keras.regularizers.l2(0.001))(X_shortcut)
+    X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+    
+    return X
+
+def ResNet18(input_shape=(64, 64, 3), classes=6):
+    """
+    Implementation of the popular ResNet18 the following architecture:
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK -> CONVBLOCK -> IDBLOCK
+    -> CONVBLOCK -> IDBLOCK -> CONVBLOCK -> IDBLOCK -> AVGPOOL -> TOPLAYER
+
+    Arguments:
+    input_shape -- shape of the images of the dataset
+    classes -- integer, number of classes
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', kernel_initializer=glorot_uniform(seed=0), kernel_regularizer = keras.regularizers.l2(0.001))(X)
+    X = BatchNormalization(axis=3, name='bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, filters=[64, 64], stage=2, block='a', s=1)
+    X = identity_block(X, [64, 64], stage=2, block='b')
+
+
+    ### START CODE HERE ###
+
+    # Stage 3 (≈4 lines)
+    X = convolutional_block(X, filters = [128, 128], stage = 3, block='a', s = 2)
+    X = identity_block(X, [128, 128], stage=3, block='b')
+
+
+    # Stage 4 (≈6 lines)
+    X = convolutional_block(X, filters = [256, 256], stage = 4, block='a', s = 2)
+    X = identity_block(X, [256, 256], stage=4, block='b')
+
+
+    # Stage 5 (≈3 lines)
+    X = convolutional_block(X, filters = [512, 512], stage = 5, block='a', s = 2)
+    X = identity_block(X, [512, 512], stage=5, block='b')
+
+
+    # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+    X = AveragePooling2D((2,2), name="avg_pool")(X)
+    X = Dropout(0.3)(X)
+
+    ### END CODE HERE ###
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0), kernel_regularizer = keras.regularizers.l2(0.001))(X)
+    
+    
+    # Create model
+    model = Model(inputs = X_input, outputs = X, name='ResNet18')
+
+    return model
